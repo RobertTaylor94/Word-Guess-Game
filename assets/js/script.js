f28bb318632a6b8547f8e34add6add41fae902c7
Robert Taylor <rjt903@gmail.com>
2023-01-13 12:35:42 +0000

    Boilerplate for javascript

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+var toGuess = document.querySelector("#display");
+var wins = document.querySelector("#wins");
+var losses = document.querySelector("#losses");
+var startButton = document.querySelector("#start");
+var resetButton = document.querySelector("#reset");
+var timer = document.querySelector("#timer-count");
+
+var words = [];
+
+var wins = 0;
+var losses = 0;
+var chosenWord = "";
+var numBlanks = 0;
+var isWin = false;
+var timer;
+var timerCount;
+
+function init() {
+
+};
+
+function startgame() {
+
+};
+
+function resetGame() {
+
+};
+
+function winGame() {
+
+};
+
+function loseGame() {
+
+};
+
+function storeScores() {
+
+};
+
+function renderWord() {
+
+};
+
+function retrieveScores() {
+
+};
+
+function keypress(event) {
+
+}
+
+startButton.addEventListener()
+
+resetButton.addEventListener()
+
